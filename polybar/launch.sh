24f4e362f4dc7941d0d9f05ade7bfbd49d38c023
Consolider <christian.spector@protonmail.com>
2024-09-25 08:36:34 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,17 +9,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar top 2>&1 | tee -a /tmp/polybar.log & disown
 
-#if [[ $(xrandr -q | grep 'DVI-D-0 connected') ]]; then
-#        polybar external 2>&1 | tee -a /tmp/polybar.log & disown 
-#fi
-
 #polybar left 2>&1 | tee -a /tmp/polybar.log & disown
 #polybar center 2>&1 | tee -a /tmp/polybar.log & disown
 #polybar right 2>&1 | tee -a /tmp/polybar.log & disown
-
-#polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-#if [[ $(xrandr -q | grep 'DVI-D-0 connected') ]]; then
-#        polybar external 2>&1 | tee -a /tmp/polybar.log & disown 
-#fi
 
 echo "Polybar launched..."
